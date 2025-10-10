d8182d6fd3a5834eb0287f5bde85c8c690c9cfef
ongoing-z <code@ongoingblog.org>
2025-10-10 19:12:31 +0800

    添加注释

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplicationRunListeners.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplicationRunListeners.java
@@ -30,7 +30,7 @@ import org.springframework.core.metrics.StartupStep;
 import org.springframework.util.ReflectionUtils;
 
 /**
- * <p>监听事件的集合类。</p>
+ * <p>应用启动过程监听事件的集合类。</p>
  * A collection of {@link SpringApplicationRunListener}.
  *
  * @author Phillip Webb
